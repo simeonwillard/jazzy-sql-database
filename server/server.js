67c37f34cc87b88e3694bd2277fb37e6e51cd2c8
Simeon Willard <swillard4597@gmail.com>
2021-01-05 17:14:22 -0600

    updated the song get to get data from the server

--- server/server.js
+++ server/server.js
@@ -12,42 +12,7 @@ app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
 
-// TODO - Replace static content with a database tables
-// const artistList = [ 
-//     {
-//         name: 'Ella Fitzgerald',
-//         birthdate: '04-25-1917'
-//     },
-//     {
-//         name: 'Dave Brubeck',
-//         birthdate: '12-06-1920'
-//     },       
-//     {
-//         name: 'Miles Davis',
-//         birthdate: '05-26-1926'
-//     },
-//     {
-//         name: 'Esperanza Spalding',
-//         birthdate: '10-18-1984'
-//     },
-// ]
-// const songList = [
-//     {
-//         title: 'Take Five',
-//         length: '5:24',
-//         released: '1959-09-29'
-//     },
-//     {
-//         title: 'So What',
-//         length: '9:22',
-//         released: '1959-08-17'
-//     },
-//     {
-//         title: 'Black Gold',
-//         length: '5:17',
-//         released: '2012-02-01'
-//     }
-// ];
+
 
 app.get('/artist', (req, res) => {
     console.log(`In /songs GET`);
@@ -79,7 +44,15 @@ app.post('/artist', (req, res) => {
 
 app.get('/song', (req, res) => {
     console.log(`In /songs GET`);
-    res.send(songList);
+
+    const queryText = `SELECT * FROM "song" ORDER BY "title" DESC;`;
+    pool.query(queryText).then((result) => {
+        console.log(result);
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+    });
 });
 
 app.post('/song', (req, res) => {
